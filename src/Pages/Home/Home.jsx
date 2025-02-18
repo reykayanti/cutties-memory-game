181156c215b3df84dd8023e6915a494ed3701c0c
Retno Ekayanti <retnoekayanti15@gmail.com>
2025-02-18 14:34:18 +0700

    done~~!

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
-import {Link, useNavigate } from 'react-router-dom'
+import {Link, useLocation, useNavigate } from 'react-router-dom'
 import { assets } from '../../assets/assets'
 
 export const Home = () => {
 
+    const location = useLocation();
+
     const [showPopup, setShowPopup] = useState(false);
     const navigate = useNavigate();
+    const { dateTime, whereWeGo } = location.state || {}; 
 
     const handlePopupClick = () => {
         setShowPopup(true);
@@ -14,7 +17,12 @@ export const Home = () => {
             navigate('/board');
         }, 3000);
     }
+
+
     
+    console.log(dateTime, whereWeGo)
+
+
   return (
     <>
         
@@ -22,19 +30,27 @@ export const Home = () => {
             <>
             <div className='home-hello'>
                 <img className='tree1' src={assets.tree} alt="" />
-                <div className="title">
+                <div className={dateTime ? 'hidden' : 'title'}  >
                     <div className="floating-texta ">Lets</div>
                     <div className="floating-textb ">play</div>
                     <div className="floating-texta ">for</div>
                     <div className="floating-textb ">your</div>
                     <div className="floating-texta ">gift</div>
                 </div>
+                <div className={dateTime ? '' : 'hidden'}  >
+                    <div className="floating-texta ">We will go to {whereWeGo}</div>
+                    <div className="floating-texta ">The date and time is</div>
+                    <div className="floating-textb ">{dateTime}</div>
+                    <div className="floating-textc ">See you later</div>
+                </div>
                 <button className='startthegame' onClick={handlePopupClick}>
-                    Start
+                    {dateTime ? 'Play Again' : 'Start'}
                 </button>
             </div>
             </>
         }
+
+
         {showPopup && 
             <div className='popup'>
                 <div className='popup-direct'></div>
--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,32 +1,35 @@
 import React, { useState } from 'react'
 import './Calendar.css'
 import { assets } from '../../assets/assets';
+import { jsPDF } from 'jspdf';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Calendar = ({choose}) => {
 
     const [selectedDay, setSelectedDay] = useState("");
-    const [selectedDate, setSelectedDate] = useState("");
-    const [selectedMonth, setSelectedMonth] = useState("");
-    const [selectedYear, setSelectedYear] = useState("");
     const [selectedHour, setSelectedHour] = useState("");
-    const [selectedMinute, setSelectedMinute] = useState("");
+
+
+    const navigate = useNavigate();
 
     // Generate days of the week
-    const daysOfWeek = ["Saturday, April 15", "Sunday, April 16"];
+    const daysOfWeek = [null, "Saturday, April 15", "Sunday, April 16"];
 
-    const hourForDinner = ["06.00 PM", "07.00 PM", "08.00 PM"];
-    const hourForAquarium = ["09.00 AM", "11.00 AM", "01.00 PM"];
-    const hourForMovie = ["09.00 AM", "11.00 AM", "12.00 AM", "01.00 PM", "02.00 PM", "06.00 PM", "07.00 PM"];
-
-    
+    const hourForDinner = [null, "06.00 PM", "07.00 PM", "08.00 PM"];
+    const hourForAquarium = [null, "09.00 AM", "11.00 AM", "01.00 PM"];
+    const hourForMovie = [null, "09.00 AM", "11.00 AM", "12.00 AM", "01.00 PM", "02.00 PM", "06.00 PM", "07.00 PM"];
 
     // Generate hours and minutes options
     const hours = ['']
 
     // Function to handle date change
-    const handleDateChange = () => {
-        const dateTime = `${selectedDay}, ${selectedDate} ${months[parseInt(selectedMonth) - 1]} ${selectedYear}, ${selectedHour}:${selectedMinute}`;
-        console.log("Selected Date and Time: ", dateTime);
+    const handleClick = () => {
+        const dateTime = `${selectedDay}, ${selectedHour}`;
+        const whereWeGo = choose
+        if (selectedDay && selectedHour) {
+          navigate('/', { state: { dateTime, whereWeGo } });
+          console.log("Selected Date and Time: ", dateTime);
+        }
     };
     
   return (
@@ -43,7 +46,7 @@ const Calendar = ({choose}) => {
       <h3>Select Date and Time to go {choose}</h3>
       <div className="calendar-grid">
         <div className="calendar-item day">
-          <label>Hari: </label>
+          <label>Day: </label>
           <select  value={selectedDay} onChange={(e) => setSelectedDay(e.target.value)}>
             {daysOfWeek.map((day, index) => (
               <option key={index} value={day}>{day}</option>
@@ -52,7 +55,7 @@ const Calendar = ({choose}) => {
         </div>
 
         <div className="calendar-item jam">
-          <label>Jam: </label>
+          <label>Hour: </label>
           <select value={selectedHour} onChange={(e) => setSelectedHour(e.target.value)}>
             {
                 choose === 'Dinner' 
@@ -77,7 +80,7 @@ const Calendar = ({choose}) => {
       </div>
 
       <div>
-        <button onClick={handleDateChange} className="confirm-btn">Confirm</button>
+        <button onClick={handleClick} className="confirm-btn">Confirm</button>
       </div>
     </div>
   )
